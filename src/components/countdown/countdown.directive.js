616ed7f765d34fbc64a4b63c8bf237b6e544487d
Justin James <justin.james@intel.com>
2014-12-13 15:55:47 -0700

    added countdown classes

--- src/components/countdown/countdown.directive.js
+++ src/components/countdown/countdown.directive.js
@@ -1,26 +1,28 @@
 angular.module('elfSantaHolidayJs')
   .directive('countdown',function($rootScope) {
+    "use strict";
+
     return {
       restrict: 'E',
       scope: {},
       templateUrl: 'components/countdown/countdown.html',
       link: function(scope,element,attrs){
        	scope.countdownCount = 60;
-        console.log(scope.countdownCount);
-        var countTimer = setInterval(function(){
-            console.log(scope.countdownCount);
-
-           if (scope.countdownCount === 0)
-           {
-             scope.countdownCount = 60;
-             $rootScope.$broadcast("timeExpired");
-           }
-           else
-           {
-             scope.countdownCount--;
-             scope.$digest();
-           }
-
+        scope.countClass = '';
+          setInterval(function(){
+             if (scope.countdownCount === 0) {
+               scope.countdownCount = 60;
+               scope.countClass = '';
+               $rootScope.$broadcast('timeExpired');
+             }
+             else {
+               scope.countdownCount--;
+               if (scope.countdownCount === 10)
+               {
+                  scope.countClass = 'red';
+               }
+               scope.$digest();
+             }
           }, 1000);
 
 
