0070ceb2a6c59497a1f7a0cad8a182fdd77d8fba
kazekagegaara <mansinkal2910@yahoo.co.in>
2014-12-13 14:29:25 -0700

    safe commit for elf

--- src/components/elf/elf.directive.js
+++ src/components/elf/elf.directive.js
@@ -5,9 +5,11 @@ angular.module('elfSantaHolidayJs')
       templateUrl: 'components/elf/elf.html',
       link: function(scope,element,attrs){
       	console.log(element);
-      	element.on("keydown",function(evt){
-
-      		while()
+      	element.bind("keydown keypress",function(evt){
+      		console.log("testing");
+      		if(evt.which === 38){
+      			console.log("here");
+      		}
       	});
       }
     };
