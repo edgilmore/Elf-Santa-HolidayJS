c61cbed233835f00fc643ff5122f0e7f592a5429
santhoshrpai <santhoshrpai@gmail.com>
2014-12-13 14:24:18 -0700

    * added santa image and references

---
+++ src/components/santa/santa.directive.js
@@ -0,0 +1,10 @@
+angular.module('elfSantaHolidayJs')
+  .directive('elf',function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'components/santa/santa.html',
+      link: function(){
+
+      }
+    };
+  });
