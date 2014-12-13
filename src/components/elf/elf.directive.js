9a72a2c84e1a720c353f7a08ae643f5a8886eebe
kazekagegaara <mansinkal2910@yahoo.co.in>
2014-12-13 15:07:49 -0700

    fixing extra elf and santa in index

--- src/components/elf/elf.directive.js
+++ src/components/elf/elf.directive.js
@@ -1,16 +1,17 @@
 angular.module('elfSantaHolidayJs')
-  .directive('elf',function() {
+  .directive('elf',function($document) {
     return {
       restrict: 'E',
       templateUrl: 'components/elf/elf.html',
       link: function(scope,element,attrs){
-      	console.log(element);
-      	element.bind("keydown keypress",function(evt){
-      		console.log("testing");
+      	console.log("in link");
+      	$document.on('keydown',function(evt){
       		if(evt.which === 38){
       			console.log("here");
+      			element[0].style.top = element[0].style.top + "5px";
       		}
       	});
+      	scope.$destroy($document.off);
       }
     };
   });
