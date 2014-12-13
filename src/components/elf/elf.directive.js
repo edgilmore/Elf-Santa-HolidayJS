f3d7fce3a78c14180df15a8ad5b0b57641fe43a2
kazekagegaara <mansinkal2910@yahoo.co.in>
2014-12-13 15:27:07 -0700

    adding elf changes

--- src/components/elf/elf.directive.js
+++ src/components/elf/elf.directive.js
@@ -4,11 +4,11 @@ angular.module('elfSantaHolidayJs')
       restrict: 'E',
       templateUrl: 'components/elf/elf.html',
       link: function(scope,element,attrs){
-      	console.log("in link");
       	$document.on('keydown',function(evt){
       		if(evt.which === 38){
-      			console.log("here");
-      			element[0].style.top = element[0].style.top + "5px";
+      			console.log(element);
+      			element[0].style.bottom = element[0].style.bottom + "5px";
+      			scope.$digest();
       		}
       	});
       	scope.$destroy($document.off);
